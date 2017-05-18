eb190e3a0946970ee18f2d966fe2d8e2268e537b
Laurvin <github@dbbv.com>
2017-05-18 21:08:20 +0200

    Create Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js

---
+++ Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name Fallen London - Show Cards Waiting in Tab
+// @namespace Fallen London - Show Cards Waiting in Tab
+// @author Laurvin
+// @description Shows the Cards Waiting text displayed under the Deck in the page title/tab instead of the default "Fallen London!".
+// @version 1.1
+// @icon http://i.imgur.com/XYzKXzK.png
+// @downloadURL xxx
+// @include http://fallenlondon.storynexus.com/Gap/*
+// @grant none
+// @run-at document-idle
+// ==/UserScript==
+
+setInterval(function() {
+    
+  if (document.getElementsByClassName("deck-contents-description").length != 0)
+  {
+    var Deck = document.getElementsByClassName('deck-contents-description')[0].innerHTML;
+    
+    var DeckShort = Deck.substr(0, 17);
+    
+    document.title = DeckShort;
+  }
+  else
+  {
+    document.title = "No deck found!";
+  }
+
+}, 60 * 1000); // 1 minute
