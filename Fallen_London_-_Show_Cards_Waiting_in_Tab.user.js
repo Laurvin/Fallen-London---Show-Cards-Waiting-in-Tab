d4986c3cfc86fe7bce22d075539f4d6d5dad2227
Laurvin <github@dbbv.com>
2018-06-06 10:10:20 +0200

    Update Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js

--- Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js
+++ Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js
@@ -2,28 +2,29 @@
 // @name Fallen London - Show Cards Waiting in Tab
 // @namespace Fallen London - Show Cards Waiting in Tab
 // @author Laurvin
-// @description Shows the Cards Waiting text displayed under the Deck in the page title/tab instead of the default "Fallen London!".
-// @version 1.1
+// @description Shows the number of cards waiting in the page title/tab instead of the default: "Fallen London".
+// @version 2.0
 // @icon http://i.imgur.com/XYzKXzK.png
 // @downloadURL https://github.com/Laurvin/Fallen-London---Show-Cards-Waiting-in-Tab/raw/master/Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js
-// @include http://fallenlondon.storynexus.com/Gap/*
+// @include https://fallenlondon.com/
+// @include https://www.fallenlondon.com/
 // @grant none
 // @run-at document-idle
 // ==/UserScript==
 
-setInterval(function() {
-    
-  if (document.getElementsByClassName("deck-contents-description").length != 0)
+function getCards()
+{
+  if (document.getElementsByClassName("deck-info__cards-in-deck").length != 0)
   {
-    var Deck = document.getElementsByClassName('deck-contents-description')[0].innerHTML;
+    var Deck = document.getElementsByClassName('deck-info__cards-in-deck')[0].innerHTML;
     
-    var DeckShort = Deck.substr(0, 17);
-    
-    document.title = DeckShort;
+    document.title = Deck;
   }
   else
   {
     document.title = "No deck found!";
   }
+  setTimeout(getCards, 30 * 1000); // 30 seconds
+}
 
-}, 60 * 1000); // 1 minute
+setTimeout(getCards, 30 * 1000);
