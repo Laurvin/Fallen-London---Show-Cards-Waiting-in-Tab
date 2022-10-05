00f2a28b71c62f05e7b64139e48dd6fef06c7cbf
Laurvin <github@dbbv.com>
2022-10-05 15:13:38 +0200

    Update Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js

--- Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js
+++ Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js
@@ -3,11 +3,12 @@
 // @namespace Fallen London - Show Cards Waiting in Tab
 // @author Laurvin
 // @description Shows the number of cards waiting in the page title/tab instead of the default: "Fallen London".
-// @version 2.1
+// @version 2.1.1
 // @icon http://i.imgur.com/XYzKXzK.png
 // @downloadURL https://github.com/Laurvin/Fallen-London---Show-Cards-Waiting-in-Tab/raw/master/Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js
-// @include https://fallenlondon.com/
-// @include https://www.fallenlondon.com/
+// @updateURL https://github.com/Laurvin/Fallen-London---Show-Cards-Waiting-in-Tab/raw/master/Fallen_London_-_Show_Cards_Waiting_in_Tab.user.js
+// @match https://fallenlondon.com/
+// @match https://www.fallenlondon.com/
 // @grant none
 // @run-at document-idle
 // ==/UserScript==
